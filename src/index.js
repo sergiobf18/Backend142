1372ca9b521cfe0a0ffb1c7cbcfe10dbbc5a9768
sergiobf18 <sergiobf@gmail.com>
2024-08-24 11:06:03 -0500

    port

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
-const express = require ('express');
+const express = require('express');
 const conectarDB = require('../configdb/db');
 const cors = require('cors');
-const port =  process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
-
-// llamamos a nuestra función conectarDB
+// Llamamos a nuestra función conectarDB
 conectarDB();
 app.use(cors());
 app.use(express.json());
 
-//rutas del proyecto
-app.use('/api/clientes', require('../routers/ClientesRutas'))
-app.use('/api/proveedores', require('../routers/ProveedoresRutas'))
+// Rutas del proyecto
+app.use('/api/clientes', require('../routers/ClientesRutas'));
+app.use('/api/proveedores', require('../routers/ProveedoresRutas'));
 
-// ruta desde la web
+// Ruta desde la web
 app.get('/', (req, res) => {
     res.send("Bienvenidos estamos desde el navegador");
-})
+});
 
-app.listen(puerto,() => console.log("Estamos conectados desde el servidor", puerto));
+app.listen(port, () => console.log("Estamos conectados desde el servidor", port));
